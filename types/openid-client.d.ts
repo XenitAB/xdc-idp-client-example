c34cbc3cfffbe3a2772af4d78ab11322a0f8e4ae
Ulrik Strid <ulrik.strid@xenit.se>
2019-01-08 13:27:16 +0100

    Add minimum types for openid-client

---
+++ types/openid-client.d.ts
@@ -0,0 +1,43 @@
+declare module "openid-client" {
+  import passport = require("passport");
+
+  class Client {
+    constructor(a: {
+      redirect_uri: string;
+      client_id: string;
+      token_endpoint_auth_method: string;
+      client_secret: string;
+    });
+  }
+
+  export class Issuer {
+    static discover(
+      url: string
+    ): {
+      Client: typeof Client;
+    };
+  }
+
+  interface StrategyConfig {
+    client: Client;
+    params: {
+      client_id: string;
+      redirect_uri: string;
+      scope: string;
+      response_type: string;
+      state: string;
+    };
+  }
+
+  interface StrategyCallback {
+    (
+      tokenset: any,
+      userinfo: any,
+      done: (e: Error | null, payload: any) => any
+    ): any;
+  }
+
+  export class Strategy extends passport.Strategy {
+    constructor(config: StrategyConfig, callback: StrategyCallback);
+  }
+}
